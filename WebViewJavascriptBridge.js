c92194839b60e7c3cee26e6c3b155805ff99a367
fxin-GitHub <hellofxin@gmail.com>
2017-06-05 15:26:37 +0800

    20170605-15:26

--- WebViewJavascriptBridge.js
+++ WebViewJavascriptBridge.js
@@ -1,6 +1,3 @@
-//notation: js file can only use this kind of comments
-//since comments will cause error when use in webview.loadurl,
-//comments will be remove by java use regexp
 (function() {
     if (window.WebViewJavascriptBridge) {
         return;
